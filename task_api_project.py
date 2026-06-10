43e70900cf74fcdbfda3e8be1fa1f8741bb0bf71
vedant <ved.csbs@gmail.com>
2026-06-10 16:16:41 +0530

    feat: complete postgresql integration for patch and delete endpoints

--- task_api_project.py
+++ task_api_project.py
@@ -65,22 +65,28 @@ def create_task(task: Task, session: Session = Depends(get_session)):
     session.refresh(task)
     return task
 
-@app.patch("/task/update-task/{task_id}")
-def update_task(task_id: int, task: TaskUpdate):
-    if task_id not in task_list:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
+@app.patch("/task/update-task/{task_id}", response_model = Task)
+def update_task(task_id: int, task: TaskUpdate, session: Session = Depends(get_session)):
+    db_task = session.get(Task, task_id)
+    if not db_task:
+        raise HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
     
-    update_data = task.model_dump(exclude_unset=True)
+    update_task = task.model_dump(exclude_unset=True)
+    for key, value in update_task.items():
+        setattr(db_task, key, value)
     
-    for key, value in update_data.items():
-        task_list[task_id][key] = value
-        
-    return task_list[task_id]
+    session.add(db_task)
+    session.commit()
+    session.refresh(db_task)
+    return db_task
+
 
 @app.delete("/task/delete-task/{task_id}")
-def delete_task(task_id : int):
-    if task_id not in task_list:
+def delete_task(task_id : int,session:Session = Depends(get_session)):
+    task = session.get(Task,task_id)
+    if not task:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found")
         
-    del task_list[task_id]
+    session.delete(task)
+    session.commit()
     return {"message" : f"Task {task_id} deleted successfully"}
