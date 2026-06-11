1a0117dc8baa60b1ad4d0c625786a9c72d5ad89d
vedant <ved.csbs@gmail.com>
2026-06-11 18:16:07 +0530

    Update backend

--- task_api_project.py
+++ task_api_project.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, status,Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session,select
 from database import get_session
 import time
@@ -20,6 +21,19 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(title="Task Management API",lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TaskCreate(BaseModel):
